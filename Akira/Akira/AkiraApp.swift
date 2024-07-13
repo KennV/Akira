831d7c82f7488af36b4c4262c370a329d8da3568
KennV <kenn.villegas@me.com>
2024-07-13 17:32:56 -0400

    INITIAL COMMIT OF MVP STATE
    Added Boilerplate project and README
    Gave it a first run

---
+++ Akira/Akira/AkiraApp.swift
@@ -0,0 +1,17 @@
+/**
+	AkiraApp.swift
+	Akira
+	Created by Kenn Villegas on 7/13/24.
+ */
+
+import SwiftUI
+
+@main
+struct AkiraApp: App {
+	var body: some Scene {
+		WindowGroup {
+			KDVMainView()
+		}
+	}
+}
+
---
+++ Akira/Akira/Views/KDVMainView.swift
@@ -0,0 +1,24 @@
+/**
+	ContentView.swift
+	Akira
+
+	Created by Kenn Villegas on 7/13/24.
+*/
+
+import SwiftUI
+
+struct KDVMainView: View {
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
+
+#Preview {
+	KDVMainView()
+}
