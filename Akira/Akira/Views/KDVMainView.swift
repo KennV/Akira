1ce7777b10de11f4f8f85e893f8391c04ce60b56
KennV <kenn.villegas@me.com>
2024-07-21 10:51:25 -0400

    Implemented Sorting
    Forward and reverse by toolbar

--- Akira/Akira/Views/KDVMainView.swift
+++ Akira/Akira/Views/KDVMainView.swift
@@ -10,34 +10,43 @@ import SwiftData
 import SwiftUI
 
 struct KDVMainView: View {
-	
+
 	@Environment(\.modelContext) var modelContext
 	@State private var path = [KDVPerson]()
 	@State private var searchString = ""
-//	var peopleView: KDVPeopleView?
+	@State private var sortOrder = [SortDescriptor (\KDVPerson.firstName)]
 
 	var body: some View {
 		NavigationStack(path: $path) {
-			KDVPeopleView(searchText: searchString)
+			KDVPeopleView(searchText: searchString, sortOrder: sortOrder)
 			.navigationTitle("Titties!")
-			.navigationDestination(for: KDVPerson.self) { 
+			.navigationDestination(for: KDVPerson.self) {
 				person in KDVEditPersonView(person: person)
 			}
 			.toolbar {
+				Menu ("Sort", systemImage: "arrow.up.arrow.down") {
+					Picker("Sort", selection: $sortOrder) {
+						Text("Name (A-Z)")
+							.tag([SortDescriptor(\KDVPerson.firstName)])
+
+						Text("Name (Z-A)")
+							.tag([SortDescriptor(\KDVPerson.firstName, order: .reverse)])
+					}
+				}
 				Button ("add person", systemImage: "plus", action: addPerson)
 					.searchable(text: $searchString)
 			}
 		}
 	}
-	
+
 	func addPerson() {
 		let newPerson = KDVPerson(userID: "unset", firstName: "nil", midOrNoName: "nil", lastName: "nil", userDetails: "unset", userAchievement: "unset", eMailAddress: "Jive")
 	newPerson.randomizePersonData(newPerson)
 		modelContext.insert(newPerson)
 		path.append(newPerson)
 	}
-
 }
+
 
 #Preview {
 	KDVMainView()
